50cd795cba55ed5f07ad4a806ef4efa71df74480
Raka989 <rakadon989@gmail.com>
2025-06-07 05:10:15 +0530

    Set homepage to profile page, fixed frontend, and added Vite build

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/', [App\Http\Controllers\front\indexcontroller::class, 'home']);
 
-Route::view('/', 'welcome');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
